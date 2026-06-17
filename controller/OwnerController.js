e9cbec799eb56baef6c1633f43cdc4814586c3a1
Poovarasan <poopoovarasan432@gmail.com>
2026-06-17 22:08:48 +0530

    logout controller modified

--- controller/OwnerController.js
+++ controller/OwnerController.js
@@ -114,6 +114,7 @@ exports.OwnerLogoutController = async (req, res) => {
   try {
     // res.status(200).cookie("jwt","",{maxAge:1000*1}).json("logout Succes")
     res.clearCookie("jwt").json({
+      sucess:true,
       message:"Logout Succes"
     });
   } catch (error) {
